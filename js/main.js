e90fd21bb4868e8aeb1e9798cdc13ac369ac999e
Aleksei Tiumentsev <alexburt@mail.ru>
2026-04-24 15:11:17 +0900

    del smoth behavior on js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
 const yearNode = document.getElementById("year");
 yearNode.textContent = new Date().getFullYear();
 
-document.querySelectorAll('a[href^="#"]').forEach((link) => {
-	link.addEventListener("click", (event) => {
-		const targetId = link.getAttribute("href");
-		if (!targetId || targetId === "#") return;
-		const target = document.querySelector(targetId);
-		if (!target) return;
-		event.preventDefault();
-		target.scrollIntoView({
-			behavior: "smooth",
-			block: "start",
-		});
-	});
-});
